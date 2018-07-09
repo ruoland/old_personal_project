a378876d90af9f5b748918aeb5e16a55d41797d6
BuildTools <unconfigured@null.spigotmc.org>
2018-07-10 00:12:27 +0900

    마인런 엘리트라 작동하게함 좀더 수정 필요

--- src/main/java/ruo/minigame/minigame/minerun/MineRun.java
+++ src/main/java/ruo/minigame/minigame/minerun/MineRun.java
@@ -8,7 +8,10 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import org.lwjgl.input.Keyboard;
@@ -32,20 +35,30 @@ public class MineRun extends AbstractMiniGame {
     public static void setElytra(int elytraMode) {
         elytra = elytraMode;
         EntityPlayer player = WorldAPI.getPlayerMP();
+        if(player == null)
+            return;
         double yaw = player.getHorizontalFacing().getHorizontalAngle();
 
-        if(elytraMode == 1 || elytraMode == 2){
-            player.inventory.armorInventory[1] = new ItemStack(Items.ELYTRA);
+        if (elytraMode == 1 || elytraMode == 2) {
+            ItemStack itemstack = player.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
+            player.inventory.armorInventory[2] = new ItemStack(Items.ELYTRA);
         }
-        if(elytraMode == 2) {
+        if (elytraMode == 2) {
             ((EntityPlayerMP) player).setElytraFlying();
+            WorldAPI.teleport(player.posX, player.posY + 1, player.posZ);
+            player.motionY -= 0.1;
+            WorldAPI.getPlayerSP().connection.sendPacket(new CPacketEntityAction(WorldAPI.getPlayerSP(), CPacketEntityAction.Action.START_FALL_FLYING));
+            NBTTagCompound compound = new NBTTagCompound();
+            WorldAPI.getPlayerSP().writeEntityToNBT(compound);
+            compound.setBoolean("FallFlying" , true);
+            WorldAPI.getPlayerSP().readEntityFromNBT(compound);
+            System.out.println("" + player.onGround + player.motionY+WorldAPI.getPlayerSP().isElytraFlying() + WorldAPI.getPlayerMP().isElytraFlying());
             noElytra(player, yaw);
-        }
-        else if (elytraMode == 1) {
+        } else if (elytraMode == 1) {
             Camera.getCamera().moveCamera(EntityAPI.lookX(player, 2), 3, EntityAPI.lookZ(player, 2));
             float roX = EntityAPI.lookX(player, 1) == 0 ? -50 : 0;
             float roZ = EntityAPI.lookZ(player, 1) == 0 ? -50 : 0;
-            if(player.getHorizontalFacing() == EnumFacing.WEST){
+            if (player.getHorizontalFacing() == EnumFacing.WEST) {
                 roZ = Math.abs(roZ);
             }
             if (yaw < 0)
@@ -53,6 +66,7 @@ public class MineRun extends AbstractMiniGame {
             else
                 Camera.getCamera().rotateCamera(roX, yaw + 180, roZ);
         } else {
+            System.out.println("엘리트라 취소됨");
             ((EntityPlayerMP) player).clearElytraFlying();
             noElytra(player, yaw);
         }
@@ -80,9 +94,9 @@ public class MineRun extends AbstractMiniGame {
         Camera.getCamera().lockCamera(true, (float) yaw, 0);
         noElytra(player, yaw);
 
-        if(obj.length > 1 && obj[1] instanceof String[]){
+        if (obj.length > 1 && obj[1] instanceof String[]) {
             String[] str = (String[]) obj[1];
-            if(str.length > 0) {
+            if (str.length > 0) {
                 if (str[0].equals("elytra"))
                     setElytra(1);
                 if (str[0].equals("create")) {
@@ -96,6 +110,8 @@ public class MineRun extends AbstractMiniGame {
         ((MineRunEvent) event).lineZ = EntityAPI.getFacingZ(player.rotationYaw - 90);
         ((MineRunEvent) event).lineFBX = EntityAPI.lookX(player, 1);
         ((MineRunEvent) event).lineFBZ = EntityAPI.lookZ(player, 1);
+        ((MineRunEvent) event).lineY = player.posY;
+
         return super.start();
     }
 
@@ -103,13 +119,13 @@ public class MineRun extends AbstractMiniGame {
         Camera.getCamera().moveCamera(EntityAPI.lookX(player, 3), -0.9, EntityAPI.lookZ(player, 3));
         float roX = EntityAPI.lookX(player, 4) == 0 ? -20 : 0;
         float roZ = EntityAPI.lookZ(player, 4) == 0 ? -20 : 0;
-        if(roX < 0){
+        if (roX < 0) {
             roX = 20;
         }
-        if(roZ < 0){
+        if (roZ < 0) {
             roZ = 20;
         }
-        if(player.getHorizontalFacing() == EnumFacing.WEST)
+        if (player.getHorizontalFacing() == EnumFacing.WEST)
             roZ = -20;
         Camera.getCamera().rotateCamera(roX, yaw + 180, roZ);
     }
@@ -122,8 +138,8 @@ public class MineRun extends AbstractMiniGame {
         gs.keyBindForward.setKeyCode(Keyboard.KEY_W);
         gs.keyBindBack.setKeyCode(Keyboard.KEY_S);
         gs.keyBindUseItem.resetKeyBindingArrayAndHash();
-        setElytra(0);
         Camera.getCamera().reset();
+        setElytra(0);
         return super.end();
     }
 
--- src/main/java/ruo/minigame/minigame/minerun/MineRunEvent.java
+++ src/main/java/ruo/minigame/minigame/minerun/MineRunEvent.java
@@ -4,8 +4,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockBasePressurePlate;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,6 +17,7 @@ import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import org.lwjgl.input.Keyboard;
+import ruo.cmplus.cm.CommandChat;
 import ruo.cmplus.deb.DebAPI;
 import ruo.minigame.MiniGame;
 import ruo.minigame.action.ActionEffect;
@@ -27,13 +32,34 @@ public class MineRunEvent {
     //elytra 2
     public int lineUD = 0;
 
-    public double lineX, lineZ, lineFBX, lineFBZ;
+    public double lineX, lineY, lineZ, lineFBX, lineFBZ;
 
     @SubscribeEvent
     public void playerTick(PlayerTickEvent e) {
         if (!MiniGame.minerun.isStart())
             return;
-        if (e.phase == Phase.END && MiniGame.minerun.isStart()) {
+        if(e.player.isElytraFlying() || MiniGame.minerun.elytraMode() == 2) {
+            e.player.motionY = 0;
+            e.player.onGround = false;
+            if(!e.player.isElytraFlying()){
+                if(e.player instanceof EntityPlayerMP)
+                ((EntityPlayerMP) e.player).setElytraFlying();
+                if(WorldAPI.equalsHeldItem(Items.APPLE)) {
+                    e.player.onGround = false;
+                    e.player.capabilities.isFlying = true;
+                    e.player.sendPlayerAbilities();
+                }
+                e.player.motionY += 0.01;
+                WorldAPI.getPlayerSP().connection.sendPacket(new CPacketEntityAction(WorldAPI.getPlayerSP(), CPacketEntityAction.Action.START_FALL_FLYING));
+                NBTTagCompound compound = new NBTTagCompound();
+                WorldAPI.getPlayerSP().writeEntityToNBT(compound);
+                compound.setBoolean("FallFlying" , true);
+                WorldAPI.getPlayerSP().readEntityFromNBT(compound);
+                System.out.println("MP인가?" +(e.player instanceof EntityPlayerMP)+ "땅? "+e.player.onGround + "모션? "+e.player.motionY+"SP 플라잉"+WorldAPI.getPlayerSP().isElytraFlying() + WorldAPI.getPlayerMP().isElytraFlying());
+            }
+        }
+        if (e.phase == Phase.END) {
+
             if (e.player.fallDistance > 5) {
                 e.player.fallDistance = 0;
                 ActionEffect.teleportSpawnPoint(e.player);
@@ -42,8 +68,8 @@ public class MineRunEvent {
                 e.player.motionY = 0.2;
                 e.player.fallDistance = 0;
             } else {
-                e.player.motionX = EntityAPI.lookX(e.player, 0.5);
-                e.player.motionZ = EntityAPI.lookZ(e.player, 0.5);
+                e.player.motionX = EntityAPI.lookX(e.player, 0.2);
+                e.player.motionZ = EntityAPI.lookZ(e.player, 0.2);
             }
         }
     }
@@ -73,12 +99,27 @@ public class MineRunEvent {
         }
         if (MineRun.elytraMode() == 2) {
             if (lineUD < 1 && DebAPI.isKeyDown(Keyboard.KEY_W) && Keyboard.getEventKeyState()) {
-                WorldAPI.teleport(player.posX, player.posY + lineUD, player.posZ);
+                WorldAPI.teleport(player.posX, player.posY + 1, player.posZ);
                 lineUD++;
+                System.out.println(lineUD);
             }
             if (lineUD > -1 && DebAPI.isKeyDown(Keyboard.KEY_S) && Keyboard.getEventKeyState()) {
-                WorldAPI.teleport(player.posX, player.posY + lineUD, player.posZ);
+                WorldAPI.teleport(player.posX, player.posY - 1, player.posZ);
                 lineUD--;
+                System.out.println(lineUD);
+            }
+            if(DebAPI.isKeyDown(Keyboard.KEY_W) || DebAPI.isKeyDown(Keyboard.KEY_S)){
+                if(lineUD == 0)
+                WorldAPI.teleport(player.posX, player.posY + lineUD, player.posZ);
+
+            }
+            if (lineLR < 1 && DebAPI.isKeyDown(Keyboard.KEY_A) && Keyboard.getEventKeyState()) {
+                WorldAPI.teleport(player.posX + lineX, player.posY, player.posZ + lineZ);
+                lineLR++;
+            }
+            if (lineLR > -1 && DebAPI.isKeyDown(Keyboard.KEY_D) && Keyboard.getEventKeyState()) {
+                WorldAPI.teleport(player.posX - lineX, player.posY, player.posZ - lineZ);
+                lineLR--;
             }
         }
         if (MineRun.elytraMode() == 1) {
@@ -90,20 +131,24 @@ public class MineRunEvent {
                 WorldAPI.teleport(player.posX - lineFBX, player.posY, player.posZ - lineFBZ);
                 lineFB--;
             }
-        }
-        double posX = player.posX, posZ = player.posZ;
-        if(MineRun.elytraMode() == 0){
-            posX += lineX * 2;
-            posZ += lineZ * 2;
-        }
-
-        if (lineLR < 1 && DebAPI.isKeyDown(Keyboard.KEY_A) && Keyboard.getEventKeyState()) {
-            WorldAPI.teleport(posX, player.posY, posZ);
-            lineLR++;
-        }
-        if (lineLR > -1 && DebAPI.isKeyDown(Keyboard.KEY_D) && Keyboard.getEventKeyState()) {
-            WorldAPI.teleport(posX, player.posY, posZ);
-            lineLR--;
+            if (lineLR < 1 && DebAPI.isKeyDown(Keyboard.KEY_A) && Keyboard.getEventKeyState()) {
+                WorldAPI.teleport(player.posX + lineX, player.posY, player.posZ + lineZ);
+                lineLR++;
+            }
+            if (lineLR > -1 && DebAPI.isKeyDown(Keyboard.KEY_D) && Keyboard.getEventKeyState()) {
+                WorldAPI.teleport(player.posX - lineX, player.posY, player.posZ - lineZ);
+                lineLR--;
+            }
+        } else if(MineRun.elytraMode() == 0){
+            if (lineLR < 1 && DebAPI.isKeyDown(Keyboard.KEY_A) && Keyboard.getEventKeyState()) {
+                WorldAPI.teleport(player.posX +  (lineX * 2), player.posY, player.posZ +  (lineZ * 2));
+                lineLR++;
+            }
+            if (lineLR > -1 && DebAPI.isKeyDown(Keyboard.KEY_D) && Keyboard.getEventKeyState()) {
+                WorldAPI.teleport(player.posX -  (lineX * 2), player.posY, player.posZ -  (lineZ * 2));
+                lineLR--;
+            }
+            System.out.print(lineLR);
         }
     }
 
