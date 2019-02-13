dd9fb796d0206768e2dc7a58af5f8c6dd73e5483
Admin <oprond@gmail.com>
2019-02-13 11:59:48 +0900

    리스폰 좀비가 잘 일어나게 조금 건듬

--- src/main/java/map/puzzle/dungeon/EntityRespawnZombie.java
+++ src/main/java/map/puzzle/dungeon/EntityRespawnZombie.java
@@ -1,5 +1,7 @@
 package map.puzzle.dungeon;
 
+import minigameLib.effect.AbstractTick;
+import minigameLib.effect.TickRegister;
 import minigameLib.map.EntityDefaultNPC;
 import minigameLib.map.EnumModel;
 import minigameLib.map.TypeModel;
@@ -7,12 +9,12 @@ import net.minecraft.entity.ai.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class EntityRespawnZombie extends EntityDefaultNPC {
     public EntityRespawnZombie(World worldIn) {
         super(worldIn);
         typeModel = TypeModel.ZOMBIE;
-
     }
 
     @Override
@@ -27,18 +29,59 @@ public class EntityRespawnZombie extends EntityDefaultNPC {
 
     @Override
     public boolean attackEntityFrom(DamageSource source, float amount) {
-        if(getHealth() - amount <= 0){
+        if (getHealth() - amount <= 0) {
             setCorpse();
             setHealth(10);
-            setSize(3,1);
+            setSize(3, 1);
         }
         return super.attackEntityFrom(source, amount);
 
     }
 
-    public void setCorpse(){
-        setXYZ(EnumModel.ROTATION, 90, 0,90);
-        setXYZ(EnumModel.TRANSLATION,0,1.3F,0);
+    public void setCorpse() {
+        setXYZ(EnumModel.ROTATION, 90, 0, 90);
+        setXYZ(EnumModel.TRANSLATION, 0, 1.3F, 0);
         setSturn(200);
+    }
+
+    @Override
+    public void onSturn() {
+        super.onSturn();
+        if (!isSturn() && isServerWorld()) {
+            TickRegister.register(new AbstractTick(TickEvent.Type.SERVER, 1, true) {
+                @Override
+                public boolean stopCondition() {
+                    return isDead;
+                }
+
+                float a = 0;
+                @Override
+                public void run(TickEvent.Type type) {
+                    a += 1F / 5F;
+                    setXYZ(EnumModel.ROTATION, 90F-a, 0, 90F-a);
+                    if (a >= 90F) {
+                        stopTick();
+                    }
+                }
+            });
+            TickRegister.register(new AbstractTick(TickEvent.Type.SERVER, 1, true) {
+                float a = 0;
+
+                @Override
+                public boolean stopCondition() {
+                    return isDead;
+                }
+
+                @Override
+                public void run(TickEvent.Type type) {
+                    a += 0.01F / 1.3F;
+                    setXYZ(EnumModel.TRANSLATION, 0, 1.3F-a, 0);
+                    if (a >= 1.3F) {
+                        stopTick();
+                    }
+                }
+            });
+        }
+
     }
 }
