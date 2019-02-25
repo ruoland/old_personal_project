32b625502a4fc43667175d33ed7e4a2a1bb0d995
opron <oprond@gmail.com>
2019-02-25 23:59:44 +0900

    마인런에 안개가 다가오는 명령어 추가

--- src/main/java/minigameLib/command/CommandMineRun.java
+++ src/main/java/minigameLib/command/CommandMineRun.java
@@ -1,69 +1,84 @@
 package minigameLib.command;
 
+import cmplus.util.Sky;
+import map.lopre2.Loop;
 import minigameLib.MiniGame;
 import minigameLib.api.WorldAPI;
+import minigameLib.effect.AbstractTick;
+import minigameLib.effect.TickRegister;
 import minigameLib.fakeplayer.FakePlayerHelper;
 import minigameLib.minigame.minerun.MineRun;
 import minigameLib.minigame.minerun.MineRunEvent;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.NumberInvalidException;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class CommandMineRun extends CommandBase {
 
-	@Override
-	public String getCommandName() {
-		return "minerun";
+    @Override
+    public String getCommandName() {
+        return "minerun";
 
-	}
+    }
 
-	@Override
-	public String getCommandUsage(ICommandSender sender) {
-		
-		return "";
-	}
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
 
-	@Override
-	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		if(args.length > 0){
-			if(args[0].equals("fake")){
-				FakePlayerHelper.spawnFakePlayer(false);
-				return;
-			}
-			if(args[0].equals("dead")){
-				FakePlayerHelper.setFakeDead();
-				return;
-			}
-			if(args[0].equals("left")){
-				EntityPlayer player = (EntityPlayer) sender;
-				WorldAPI.teleport(player.posX, player.posY, player.posZ, player.getHorizontalFacing().getHorizontalAngle()+90, 70);
-				MiniGame.minerun.start(sender, args);
-				return;
-			}
-			if(args[0].equals("right")){
-				EntityPlayer player = (EntityPlayer) sender;
-				WorldAPI.teleport(player.posX, player.posY, player.posZ, player.getHorizontalFacing().getHorizontalAngle()-90, 70);
-				MiniGame.minerun.start(sender, args);
-				return;
-			}
-			if(args[0].equals("lava")){
-				for(int i = 0; i < MineRun.removeLavaState.size();i++){
-					sender.getEntityWorld().setBlockState(MineRun.removeLavaPos.get(i), MineRun.removeLavaState.get(i));
-				}
-				return;
-			}
-			if(args[0].equals("respawn")){
-				MineRunEvent.respawnTime = 61;
-				WorldAPI.command("/minerun lava");
-				return;
-			}
-		}
-		if(args.length > 0 && (args[0].equalsIgnoreCase("end") || args[0].equalsIgnoreCase("stop")))
-			MiniGame.minerun.end();
-		else
-			MiniGame.minerun.start(sender, args);
-	}
+        return "";
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        if (args.length > 0) {
+            if (args[0].equals("fog") && args.length > 1) {
+                Sky.fogDistance(-1);
+                TickRegister.register(new AbstractTick(1, true) {
+                    @Override
+                    public void run(TickEvent.Type type) {
+                        Sky.fogDistance(Sky.getFogDistance() - 0.5F);
+                        try {
+                            if (Sky.getFogDistance() <= parseInt(args[1])) {
+                                absLoop = false;
+                            }
+                        } catch (NumberInvalidException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                return;
+            }
+            if (args[0].equals("left")) {
+                EntityPlayer player = (EntityPlayer) sender;
+                WorldAPI.teleport(player.posX, player.posY, player.posZ, player.getHorizontalFacing().getHorizontalAngle() + 90, 70);
+                MiniGame.minerun.start(sender, args);
+                return;
+            }
+            if (args[0].equals("right")) {
+                EntityPlayer player = (EntityPlayer) sender;
+                WorldAPI.teleport(player.posX, player.posY, player.posZ, player.getHorizontalFacing().getHorizontalAngle() - 90, 70);
+                MiniGame.minerun.start(sender, args);
+                return;
+            }
+            if (args[0].equals("lava")) {
+                for (int i = 0; i < MineRun.removeLavaState.size(); i++) {
+                    sender.getEntityWorld().setBlockState(MineRun.removeLavaPos.get(i), MineRun.removeLavaState.get(i));
+                }
+                return;
+            }
+            if (args[0].equals("respawn")) {
+                MineRunEvent.respawnTime = 61;
+                WorldAPI.command("/minerun lava");
+                return;
+            }
+        }
+        if (args.length > 0 && (args[0].equalsIgnoreCase("end") || args[0].equalsIgnoreCase("stop")))
+            MiniGame.minerun.end();
+        else
+            MiniGame.minerun.start(sender, args);
+    }
 
 }
